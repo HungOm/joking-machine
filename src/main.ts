5b89d3f1459c08d34ae6a5c1622bbee62ef8ed06
Om <hungomoct19@outlook.com>
2021-10-15 11:23:56 +0800

    complete -refactored

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import './style.css'
+import { VoiceRSS } from './vocie.min';
+const button = document.querySelector('#button') as HTMLButtonElement;
+const audio = document.querySelector('#audio') as HTMLAudioElement;
+const jokeUrl:string = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist,explicit";
+
+
+async function getJoke(url:string){
+  let joke:string|undefined = '';
+  try{
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    if(data.setup){
+      joke = `${data.setup} ... ${data.delivery}`
+    }else{
+      joke = data.joke
+    }
+    await tellJoke(joke);
+
+    //disable button
+    toggleButton()
+  }catch(err){
+    console.log(err)
+  }
+}
+
+
+//toggle the button
+
+function toggleButton():void{
+  button.disabled = !button.disabled;
+
+}
+async function tellJoke(joke:string|undefined){
+  if(joke==undefined){
+    joke = "Sorry, I cant get my joke now for some unattainable reason"
+  }
+  VoiceRSS.speech({
+    key: 'c0480b4cd0ac4d5998137c7756129a81',
+    src: joke,
+    hl: 'en-us',
+    v: 'Mary',
+    r: 0, 
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+    audio:audio
+});
+}
+button.addEventListener('click',()=>{
+  getJoke(jokeUrl)
+})
+
+audio.addEventListener('ended',toggleButton)
